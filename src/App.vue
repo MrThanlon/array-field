bf6fe36bacfe9c43622913fc1acdc59d4bf5778c
MrThanlon <i@anclx.cc>
2022-01-21 20:41:44 +0800

    update: copyright and license
    Signed-off-by: MrThanlon <i@anclx.cc>

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <router-link to="/2d">2D</router-link>
   |
   <router-link to="/3d">3D</router-link>
+  |
+  &copy;<a href="https://github.com/MrThanlon/array-field" target="_blank">array-field</a>
+  |
+  License <a href="https://www.gnu.org/licenses/agpl-3.0.html" target="_blank">AGPLv3</a>
   <router-view/>
 </template>
 
